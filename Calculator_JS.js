2b2c2a1beff1fafe07120683d4d77cac73805cc3
Tollkien <AustinJBreaux@outlook.com>
2022-12-16 14:13:10 -0600

    Creates notes for project goals on CSS, HTML, and JS files

---
+++ Calculator_JS.js
@@ -0,0 +1,23 @@
+/*
+Notes:
+Create all calculator operations:
+    Add
+    Subtract
+    Multiple
+    Divide
+    Operate - Takes an operator (like the above) and 2 numbers, then uses that
+        operator on those numbers
+    Clear - Clear all data stored
+    Decimals (Round to the 10th place, and only allow one per number)
+    Equals
+    Positive/negative integers
+    Storage of the display value for the operate button
+    users should be able to string operators (like 5 + 4 + 3 etc)
+    First number should interact directly with the second, no order of operations 
+        bullshit (So 5 + 5 - 2 * 3 = 24)
+    If an operator is called without a following number, delete the operator
+        and display the first number
+        Snarky message if someone tries to divide by 0
+    Toggle button for allowing keyboard controls
+    Backspace button
+*/
