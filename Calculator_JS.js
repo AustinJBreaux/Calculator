48589539843861c8cd8739877bac82b042da0ebe
Tollkien <AustinJBreaux@outlook.com>
2023-02-25 21:10:10 -0600

    Removes the debug test button and refactors code for readability

--- Calculator_JS.js
+++ Calculator_JS.js
@@ -1,25 +1,8 @@
 //Global Variables
 let displayWindow = '';
-let numberRegExp = [0-9];
-let operatorRegExp = [/\+\/\*/i]
 let displayWindowHTML = document.querySelector('.displayWindow');
 
 //Button Functions
-const testButtonClick = function testButtonClick(e){//Debug
-    if(e.target.classList != 'testButton'){
-        return
-    }
-    else if(e.target.classList == 'testButton'){
-        let buttonPresses = '256*978+65412-69874/256';//Add here
-        let splitTest = buttonPresses.split('');
-        splitTest.forEach((element, index) => {
-            splitTest.splice(index, 1, `e${element}`)
-        });
-        displayWindow = splitTest.join('');
-        console.log(`Pressing ${buttonPresses}`);
-    }
-    else return
-}
 const numberClick = function numberClick(e){
     if(e.target.classList != 'numberButton'){
         return
@@ -30,7 +13,7 @@ const numberClick = function numberClick(e){
         displayWindowHTML.innerHTML += toInsert;
     }
     
-    else if(displayWindow[displayWindow.length-1] = numberRegExp){
+    else if(displayWindow[displayWindow.length-1] = [0-9]){
         let toInsert = e.target.innerText;
         displayWindow = displayWindow + `e${toInsert}`;
         displayWindowHTML.innerHTML += toInsert;
@@ -38,7 +21,6 @@ const numberClick = function numberClick(e){
     
     else return
 }
-
 const operatorClick = function operatorClick(e){
     if(e.target.classList != 'operatorButton'){
         return
@@ -49,7 +31,6 @@ const operatorClick = function operatorClick(e){
         displayWindowHTML.innerHTML += toInsert;
     }
 }
-
 const clearButtonClick = function clearButtonClick(e){
     if(e.target.classList != 'clearButton'){
         return
@@ -60,7 +41,6 @@ const clearButtonClick = function clearButtonClick(e){
         console.clear();
     }
 }
-
 const backspaceButtonClick = function backspaceButtonClick(e){
     if(e.target.classList != 'backspaceButton'){
         return
@@ -70,7 +50,6 @@ const backspaceButtonClick = function backspaceButtonClick(e){
         displayWindowHTML.innerHTML = displayWindowHTML.innerHTML.slice(0, -1);
     }
 }
-
 const signChangeButtonClick = function signChangeButtonClick(e){
     if(e.target.classList != 'signChangeButton'){
         return
@@ -82,7 +61,6 @@ const signChangeButtonClick = function signChangeButtonClick(e){
         displayWindowHTML.innerHTML += " -";
     }
  }
-
 const decimalButtonClick = function decimalButtonClick(e){
     if(e.target.classList != 'decimalButton'){
         return
@@ -93,7 +71,6 @@ const decimalButtonClick = function decimalButtonClick(e){
         displayWindowHTML.innerHTML += toInsert;
     }
  }
-
  const enterButtonClick = function enterButtonClick(e){
     if(e.target.classList != 'enterButton'){
         return
@@ -109,11 +86,6 @@ const decimalButtonClick = function decimalButtonClick(e){
         console.log(displayWindowArray);
     }
  }
-
- //Error Functions
- //All error functions have been removed, rely on the user to figure out why it's erroring
- //They're in a notepad in the calculator folder
- 
 const checkDividingByZero = function dividingByZero(displayWindowArray){
     displayWindowArray.forEach((element, index) => {
         if(element == "/" && displayWindowArray[index+1] == "0"){
@@ -134,9 +106,10 @@ const dataFunctions = function dataFunctions(displayWindowArray){
 }
 const combineNumbers = function combineNumbers(displayWindowArray){
     /*Note: 
-        This function can cause an error if any single number is greater than 512 numbers long. 
-        This character length can be extended by increasing the number after the
-        "displayWindowArray.length+___" below (currently 100), but this shouldn't be a problem
+        This function can cause a bug if any single number is greater than 512 numbers long.
+        This shouldn't be a problem in normal operations, but if necessary can be 
+        extended by increasing the number after the "displayWindowArray.length+___" in
+        the for loop below (Default is 100).
     */
     for(let counter = 0; counter < displayWindowArray.length+100; counter++){
         displayWindowArray.forEach((element, index) => {
@@ -236,7 +209,6 @@ const finalEvaluations = function finalEvaluations(displayWindowArray){
     console.log(displayWindowArray);
 }
 
-
 //Event Listeners
 document.addEventListener("click", numberClick);
 document.addEventListener("click", operatorClick);
@@ -245,4 +217,3 @@ document.addEventListener("click", backspaceButtonClick);
 document.addEventListener("click", signChangeButtonClick);
 document.addEventListener("click", decimalButtonClick);
 document.addEventListener("click", enterButtonClick);
-document.addEventListener("click", testButtonClick);
