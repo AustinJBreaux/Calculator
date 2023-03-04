ac3dbc28c67bec338689310b077b77e90ef87a53
Tollkien <AustinJBreaux@outlook.com>
2023-03-04 17:57:29 -0600

    Adds keyboard support for all number/operator keys, the backspace button, and the clear button

--- Calculator_JS.js
+++ Calculator_JS.js
@@ -3,110 +3,126 @@ let displayWindow = '';
 let displayWindowHTML = document.querySelector('.displayWindow');
 
 //Button Functions
-const numberClick = function numberClick(e){
-    if(e.target.classList != 'numberButton'){
+const numberClick = function numberClick(e) {
+    if (e.target.classList != 'numberButton') {
         return
     }
-    else if (displayWindow === undefined || displayWindow.length == 0){
+    else if (displayWindow === undefined || displayWindow.length == 0) {
         let toInsert = e.target.innerText;
         displayWindow = displayWindow + `e${toInsert}`;
         displayWindowHTML.innerHTML += toInsert;
     }
-    
-    else if(displayWindow[displayWindow.length-1] = [0-9]){
+
+    else if (displayWindow[displayWindow.length - 1] = [0 - 9]) {
         let toInsert = e.target.innerText;
         displayWindow = displayWindow + `e${toInsert}`;
         displayWindowHTML.innerHTML += toInsert;
     }
-    
+
     else return
 }
-const operatorClick = function operatorClick(e){
-    if(e.target.classList != 'operatorButton'){
+const operatorClick = function operatorClick(e) {
+    if (e.target.classList != 'operatorButton') {
         return
     }
-    else if(displayWindow.noSuchProperty === undefined){
+    else if (displayWindow.noSuchProperty === undefined) {
         let toInsert = e.target.innerText;
         displayWindow = displayWindow + `e${toInsert}`
         displayWindowHTML.innerHTML += toInsert;
     }
 }
-const clearButtonClick = function clearButtonClick(e){
-    if(e.target.classList != 'clearButton'){
+const clearButtonClick = function clearButtonClick(e) {
+    if (e.target.classList != 'clearButton') {
         return
     }
-    else if(displayWindow.noSuchProperty === undefined){
+    else if (displayWindow.noSuchProperty === undefined) {
         displayWindow = '';
         displayWindowHTML.innerHTML = '';
         console.clear();
     }
 }
-const backspaceButtonClick = function backspaceButtonClick(e){
-    if(e.target.classList != 'backspaceButton'){
+const backspaceButtonClick = function backspaceButtonClick(e) {
+    if (e.target.classList != 'backspaceButton') {
         return
     }
-    else if(displayWindow.noSuchProperty === undefined){//Working on this
-        displayWindow = displayWindow.slice(0,-1);
+    else if (displayWindow.noSuchProperty === undefined) {//Working on this
+        displayWindow = displayWindow.slice(0, -1);
         displayWindowHTML.innerHTML = displayWindowHTML.innerHTML.slice(0, -1);
     }
 }
-const signChangeButtonClick = function signChangeButtonClick(e){
-    if(e.target.classList != 'signChangeButton'){
+const signChangeButtonClick = function signChangeButtonClick(e) {
+    if (e.target.classList != 'signChangeButton') {
         return
     }
-    
-    else{
+
+    else {
         console.log(' -');
         displayWindow = displayWindow + `e -`;
         displayWindowHTML.innerHTML += " -";
     }
- }
-const decimalButtonClick = function decimalButtonClick(e){
-    if(e.target.classList != 'decimalButton'){
+}
+const decimalButtonClick = function decimalButtonClick(e) {
+    if (e.target.classList != 'decimalButton') {
         return
     }
-    else if(displayWindow.noSuchProperty === undefined){
+    else if (displayWindow.noSuchProperty === undefined) {
         let toInsert = e.target.innerText;
         displayWindow = displayWindow + `e${toInsert}`;
         displayWindowHTML.innerHTML += toInsert;
     }
- }
- const enterButtonClick = function enterButtonClick(e){
-    if(e.target.classList != 'enterButton'){
+}
+const enterButtonClick = function enterButtonClick(e) {
+    if (e.target.classList != 'enterButton') {
         return
     }
-    else if(displayWindow === ''){
+    else if (displayWindow === '') {
         return
     }
-    else if(displayWindow != undefined || ''){
+    else if (displayWindow != undefined || '') {
         let displayWindowArray = displayWindow.split('e');
         displayWindowArray = displayWindowArray.filter(displayWindowArray => displayWindowArray != "");
         checkDividingByZero(displayWindowArray);
         dataFunctions(displayWindowArray);
         console.log(displayWindowArray);
     }
- }
-const checkDividingByZero = function dividingByZero(displayWindowArray){
+}
+const checkDividingByZero = function dividingByZero(displayWindowArray) {
     displayWindowArray.forEach((element, index) => {
-        if(element == "/" && displayWindowArray[index+1] == "0"){
+        if (element == "/" && displayWindowArray[index + 1] == "0") {
             console.log("Error - Division by zero");
             return
         }
     });
 }
 
-//Keyboard Master Control
-const buttonPresses = function buttonPresses(e){
+//Keyboard Controls
+const buttonPresses = function buttonPresses(e) {
     e.preventDefault();
+    let keys = ["/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", "."];
     let checkbox = document.getElementById("keyboardSupport");
-    if(checkbox.checked == true){
-        console.log(e.key); 
+    if (checkbox.checked == true) {
+        console.log(e.key);
+        if (e.ctrlKey && e.key === "Backspace") {
+            displayWindow = '';
+            displayWindowHTML.innerHTML = '';
+            console.clear();
+        }
+        else if(e.key === "Backspace"){
+            displayWindow = displayWindow.slice(0, -1);
+            displayWindowHTML.innerHTML = displayWindowHTML.innerHTML.slice(0, -1);
+        }
+        else if (keys.includes(e.key)){
+            toInsert = e.key;
+            displayWindow = displayWindow + `e${toInsert}`;
+            displayWindowHTML.innerHTML += toInsert;
+        }
+        else return
     }
-    else console.log("False"); return
+    else return
 }
 
 //Data Functions
-const dataFunctions = function dataFunctions(displayWindowArray){
+const dataFunctions = function dataFunctions(displayWindowArray) {
     combineNumbers(displayWindowArray);
     placeholderForDecimal(displayWindowArray);
     combineDecimalNumbers(displayWindowArray);
@@ -114,106 +130,106 @@ const dataFunctions = function dataFunctions(displayWindowArray){
     signChange(displayWindowArray);
     finalEvaluations(displayWindowArray);
 }
-const combineNumbers = function combineNumbers(displayWindowArray){
+const combineNumbers = function combineNumbers(displayWindowArray) {
     /*Note: 
         This function can cause a bug if any single number is greater than 512 numbers long.
         This shouldn't be a problem in normal operations, but if necessary can be 
         extended by increasing the number after the "displayWindowArray.length+___" in
         the for loop below (Default is 100).
     */
-    for(let counter = 0; counter < displayWindowArray.length+100; counter++){
+    for (let counter = 0; counter < displayWindowArray.length + 100; counter++) {
         displayWindowArray.forEach((element, index) => {
             let test1 = Number.isInteger(Number.parseInt(element));
-            let test2 = Number.isInteger(Number.parseInt(displayWindowArray[index+1]));
+            let test2 = Number.isInteger(Number.parseInt(displayWindowArray[index + 1]));
             let combine = "";
-            if(test1 && test2){
+            if (test1 && test2) {
                 combine = combine.concat(displayWindowArray[index]);
-                combine = combine.concat(displayWindowArray[index+1]);
+                combine = combine.concat(displayWindowArray[index + 1]);
                 displayWindowArray.splice(index, 1, combine);
-                displayWindowArray.splice((index+1), 1);
-           }
-           else if(test1 && (displayWindowArray[index+1] == undefined)){
+                displayWindowArray.splice((index + 1), 1);
+            }
+            else if (test1 && (displayWindowArray[index + 1] == undefined)) {
                 combine = combine.concat(displayWindowArray[index]);
                 displayWindowArray.splice(index, 1, combine)
-           }
-           else {
-            combineNumbers
-           }
+            }
+            else {
+                combineNumbers
+            }
         });
     }
 }
-const placeholderForDecimal = function placeholderForDecimal(displayWindowArray){
+const placeholderForDecimal = function placeholderForDecimal(displayWindowArray) {
     displayWindowArray.forEach((element, index) => {
-        if(element == "." && !Number.isInteger(Number.parseInt(displayWindowArray[index-1]))){
+        if (element == "." && !Number.isInteger(Number.parseInt(displayWindowArray[index - 1]))) {
             displayWindowArray[index] = element.replace(".", "0.");
         }
     });
 }
-const combineDecimalNumbers = function combineDecimalNumbers(displayWindowArray){
+const combineDecimalNumbers = function combineDecimalNumbers(displayWindowArray) {
     displayWindowArray.forEach((element, index) => {
-        switch(element){
+        switch (element) {
             case ".":
-                if(Number.isInteger(Number.parseInt(displayWindowArray[index-1]))
-                && Number.isInteger(Number.parseInt(displayWindowArray[index+1]))){
-                    let combined = displayWindowArray[index-1].concat(displayWindowArray[index]);
-                    combined = combined.concat(displayWindowArray[index+1]);
-                    displayWindowArray.splice(index-1, 1, combined);
+                if (Number.isInteger(Number.parseInt(displayWindowArray[index - 1]))
+                    && Number.isInteger(Number.parseInt(displayWindowArray[index + 1]))) {
+                    let combined = displayWindowArray[index - 1].concat(displayWindowArray[index]);
+                    combined = combined.concat(displayWindowArray[index + 1]);
+                    displayWindowArray.splice(index - 1, 1, combined);
                     displayWindowArray.splice(index, 2);
-            }
+                }
                 else return
                 break
 
             case "0.":
-                if(Number.isInteger(Number.parseInt(displayWindowArray[index+1]))){
-                    let combined = displayWindowArray[index].concat(displayWindowArray[index+1]);
+                if (Number.isInteger(Number.parseInt(displayWindowArray[index + 1]))) {
+                    let combined = displayWindowArray[index].concat(displayWindowArray[index + 1]);
                     displayWindowArray.splice(index, 1, combined);
-                    displayWindowArray.splice((index+1), 1);
-            }
-            else return
-            break
+                    displayWindowArray.splice((index + 1), 1);
+                }
+                else return
+                break
         }
     });
 }
-const signChange = function signChange(displayWindowArray){
+const signChange = function signChange(displayWindowArray) {
     displayWindowArray.forEach((element, index) => {
         let signChangeTest = [" -"];
-        if(signChangeTest.includes(element)){
-            let combined = displayWindowArray[index].concat(displayWindowArray[index+1]);
+        if (signChangeTest.includes(element)) {
+            let combined = displayWindowArray[index].concat(displayWindowArray[index + 1]);
             displayWindowArray.splice(index, 2, combined);
         }
     });
 }
-const finalEvaluations = function finalEvaluations(displayWindowArray){
-    do{
+const finalEvaluations = function finalEvaluations(displayWindowArray) {
+    do {
         let replace = 0;
-        if(displayWindowArray[1] == "/"){
+        if (displayWindowArray[1] == "/") {
             replace = Number.parseFloat(displayWindowArray[0]) / Number.parseFloat(displayWindowArray[2]);
             replace = +replace.toFixed(2);
             displayWindowArray.splice(0, 1, replace);
             displayWindowArray.splice(1, 2);
         }
-        else if(displayWindowArray[1] == "*"){
+        else if (displayWindowArray[1] == "*") {
             replace = Number.parseFloat(displayWindowArray[0]) * Number.parseFloat(displayWindowArray[2]);
             replace = +replace.toFixed(2);
             displayWindowArray.splice(0, 1, replace);
             displayWindowArray.splice(1, 2);
         }
-        else if(displayWindowArray[1] == "+"){
+        else if (displayWindowArray[1] == "+") {
             replace = Number.parseFloat(displayWindowArray[0]) + Number.parseFloat(displayWindowArray[2]);
             replace = +replace.toFixed(2);
             displayWindowArray.splice(0, 1, replace);
             displayWindowArray.splice(1, 2);
         }
-        else if(displayWindowArray[1] == "-"){
+        else if (displayWindowArray[1] == "-") {
             replace = Number.parseFloat(displayWindowArray[0]) - Number.parseFloat(displayWindowArray[2]);
             replace = +replace.toFixed(2);
             displayWindowArray.splice(0, 1, replace);
             displayWindowArray.splice(1, 2);
         }
         else return
-        
+
     }
-    while(
+    while (
         displayWindowArray != 1
     );
     console.log(displayWindowArray);
